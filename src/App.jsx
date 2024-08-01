5860b13e05901e274ee509ef9707e7e47a19aa95
Vasu Khandelwal <f20190700@pilani.bits-pilani.ac.in>
2024-08-01 17:09:49 +0530

    added speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import "./index.css";
-import "./App.css"
-import Home from './routes/Home';
-import Project from './routes/Project';
-import Contact from './routes/Contact';
-import About from './routes/About';
+import "./App.css";
+import Home from "./routes/Home";
+import Project from "./routes/Project";
+import Contact from "./routes/Contact";
+import About from "./routes/About";
 import { Route, Routes } from "react-router-dom";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-
   return (
     <div>
-
-      <Routes>    
-        <Route path="/" element={<Home/>} />
-        <Route path="/project" element={<Project/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/contact" element={<Contact/>} />
+      <SpeedInsights />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-
     </div>
   );
 }
